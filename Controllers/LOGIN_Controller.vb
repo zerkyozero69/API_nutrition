13c70d36b2cb87ac66c49afc29bf17178ffac3b3
Anawatp <zerkyozero@gmail.com>
2019-06-28 15:31:33 +0700

    ลงได้

---
+++ Controllers/LOGIN_Controller.vb
@@ -0,0 +1,90 @@
+﻿
+Imports System.Data.SqlClient.SqlDataReader
+Imports System.IO
+Imports System.Net
+Imports System.Uri
+Imports System.Net.Http
+Imports System.Web.HttpContext
+Imports System.Web.Http
+Imports System.Web.Script.Serialization
+Imports System.Xml
+Imports Newtonsoft
+Imports System.Data
+Imports System.Configuration
+Imports Microsoft.ApplicationBlocks.Data
+Imports API_nutrition_vb.Member
+Imports API_nutrition_vb.RegisterStatus
+Imports API_nutrition_vb.nutrirtion
+Imports NTi.CommonUtility
+Imports Newtonsoft.Json
+Imports System.Data.SqlClient
+
+Namespace Controllers
+    Public Class LOGIN_Controller
+        Inherits ApiController
+        Dim strConn As String = ConfigurationManager.ConnectionStrings("APIConnection").ConnectionString
+        <Route("Login")>
+        <AcceptVerbs("P           OST")>
+        Public Function Login() As HttpResponseMessage
+
+            Try
+                'ตรวจสอบ FormData
+                Dim User_name As String = "" 'username
+
+                Dim password As String = "" 'รหัสผ่าน
+
+                If Current.Request.Form.AllKeys.Contains("UserName") = True Then
+                    User_name = Current.Request.Form("UserName").ToString()
+                End If
+
+                If Current.Request.Form.AllKeys.Contains("password") = True Then
+                    password = Current.Request.Form("password").ToString()
+                End If
+
+                If User_name <> "" And password <> "" Then
+                    Dim ds As New DataSet
+                    Dim sql As String
+                    sql = "SELECT User_Name, User_id FROM User_Register WHERE User_Name = @User_Name" 'ทำการ select sql จาก username 
+                    ds = SqlHelper.ExecuteDataset(strConn, CommandType.Text, sql, New SqlParameter("@User_Name", User_name)) 'ทำการเอ็กซีคิว แล้วเทียบพารามิตเตอร์ username 
+                    If ds.Tables(0).Rows.Count < 0 Then
+                        Dim result_user As New _member 'ประกาศผลเป็น json 
+                        result_user.User_name = User_name
+                        result_user.Token_key =
+                        result_user.Created_date =
+                        result_user.message = "เข้าระบบสำเร็จ"
+                        Return Request.CreateResponse(HttpStatusCode.OK, result_user)
+                    Else
+                        Dim err As [Error]
+                        err.code = "1" 'กรอกข้อมูลซ้ำ
+                        err.message = "ชื่อผู้ใช้งานผิด "
+                        Return Request.CreateResponse(HttpStatusCode.BadRequest, err)
+                        Dim user_info As New [RegisterStatus]
+                    End If
+
+                End If
+
+
+
+
+
+
+                    'error case เกิดข้อผิดพลาด
+                'Dim err As New [Error]
+                'err.code = "5" 'กรอกข้อมูลการสมัครไม่ครบ
+                'err.message = "กรุณาใส่ข้อมูลชื่อผู้ใช้งานและรหัสผ่านให้ครบถ้วนถูกต้อง, ห้ามเว้นว่างไว้"
+                ''Return resual
+                'Return Request.CreateResponse(HttpStatusCode.BadRequest, err)
+
+
+            Catch ex As Exception
+                'error case เกิดข้อผิดพลาด
+                Dim err As New [Error]
+                err.code = "6" 'error จากสาเหตุอื่นๆ จะมีรายละเอียดจาก system แจ้งกลับ
+                err.message = ex.Message
+                'Return resual
+                Return Request.CreateResponse(HttpStatusCode.BadRequest, err)
+            End Try
+
+        End Function
+    End Class
+End Namespace
--- Controllers/MemberController.vb
+++ Controllers/MemberController.vb
@@ -25,7 +25,7 @@ Namespace Controllers
         Inherits ApiController
         Dim strConn As String = ConfigurationManager.ConnectionStrings("APIConnection").ConnectionString
         <Route("register")>
-        <AcceptVerbs("POST")>
+        <AcceptVerbs("POST")> 'method post
         Public Function Accountregister() As HttpResponseMessage
             Try
 
@@ -34,16 +34,16 @@ Namespace Controllers
                 'Dim password As String = GetForm(HttpContext.Current, "User_Password")
                 'Dim GU_ID As String = GetForm(HttpContext.Current, "GU_ID")
                 'Dim Created_by As String = GetForm(HttpContext.Current, "Created_by")
-                Dim User_name As String = ""
-                Dim User_Password As String = ""
+                Dim User_name As String = ""  'ชื่อที่ใช้สมัคร
+                Dim User_Password As String = "" ' รหัสผ่าน
                 'Dim token As String = ""
                 'Dim GU_ID As String = Nothing
-                Dim Created_by As String = ""
+                Dim Created_by As String = "" 'สร้างโดยใคร
 
-                If Current.Request.Form.AllKeys.Contains("User_Name") = True Then
+                If Current.Request.Form.AllKeys.Contains("User_Name") = True Then 'รับข้อมูล user จาก fromdata  post
                     User_name = Current.Request.Form("User_Name").ToString()
                 End If
-                If Current.Request.Form.AllKeys.Contains("User_Password") = True Then
+                If Current.Request.Form.AllKeys.Contains("User_Password") = True Then 'รับข้อมูล พาส จาก fromdata  post
                     User_Password = Current.Request.Form("User_Password").ToString()
                 End If
                 'If Current.Request.Form.AllKeys.Contains("GU_ID") = True Then
@@ -54,31 +54,31 @@ Namespace Controllers
                 End If
 
 
-                'ตรวจสอบว่าส่งพารามิเตอร์ครบไหม              
+                'ตรวจสอบว่ามีการส่งพารามิตเตอร์            
                 Dim ds As New DataSet
                 Dim sql As String
-                sql = "SELECT User_Name, User_id FROM User_Register WHERE User_Name = @User_Name"
-                ds = SqlHelper.ExecuteDataset(strConn, CommandType.Text, sql, New SqlParameter("@User_Name", User_name))
+                sql = "SELECT User_Name, User_id FROM User_Register WHERE User_Name = @User_Name" 'ทำการ select sql จาก username 
+                ds = SqlHelper.ExecuteDataset(strConn, CommandType.Text, sql, New SqlParameter("@User_Name", User_name)) 'ทำการเอ็กซีคิว แล้วเทียบพารามิตเตอร์ username 
 
-                If ds.Tables(0).Rows.Count > 0 Then
+                If ds.Tables(0).Rows.Count > 0 Then 'กำหนดเงื่อนไข ถ้าเทเบิ้ลมีข้อมูล user ให้แจ้งว่า มีชื่อผู้ใช้งาน แต่ถ้าไม่มีให้ทำการลงทะเบียนได้
                     Dim err As [Error]
                     err.code = "1" 'กรอกข้อมูลซ้ำ
                     err.message = "ชื่อผู้ใช้งานมีในระบบอยู่แล้ว "
                     Return Request.CreateResponse(HttpStatusCode.BadRequest, err)
                 Else
 
-                    SqlHelper.ExecuteNonQuery(strConn, CommandType.StoredProcedure, "spT_User_PersonInsert",
+                    SqlHelper.ExecuteNonQuery(strConn, CommandType.StoredProcedure, "spT_User_PersonInsert",  ' ส่งพารามิเตอร์เช้ามา
                                                                       New SqlParameter("@User_Name", User_name),
                                                                       New SqlParameter("@User_Password ", User_Password),
                                                                      New SqlParameter("@Created_by ", Created_by))
                     'Return result ลงทะเบียนสำเร็จ
                     Dim dt As DataSet
                     Dim sqlG As String
-                    sqlG = "SELECT User_Name,User_Password, Token_key,Created_date FROM User_Register   ORDER BY User_ID desc"
+                    sqlG = "SELECT User_Name,User_Password, Token_key,Created_date FROM User_Register   ORDER BY User_ID desc" 'ทำการ select ข้อมูล ที่เข้าตารางเมื่อกี้
                     ds = SqlHelper.ExecuteDataset(strConn, CommandType.Text, sqlG)
                     Dim dt2 As DataTable
                     dt2 = ds.Tables(0)
-                    Dim result_user As New _member
+                    Dim result_user As New _member 'ประกาศผลเป็น json 
                     result_user.User_name = dt2.Rows(0).Item("User_Name")
                     result_user.Token_key = dt2.Rows(0).Item("Token_key")
                     result_user.Created_date = dt2.Rows(0).Item("Created_date")
@@ -89,8 +89,8 @@ Namespace Controllers
 
 
             Catch ex As Exception
-                'error case เกิดข้อผิดพลาด
-                Dim err As New [Error]
+                'error case เกิดข้อผิดพลาด 
+                Dim err As New [Error] 'ถ้ามีอยู่แล้วข้อมูลจะเด้งออก
                 err.code = "2" 'username ซ้ำ
                 err.message = "Username ซ้ำ"
                 'Return resual
